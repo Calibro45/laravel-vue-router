36e10f19da29cd839b72b52b0e0bb57bb618ced7
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-05-05 10:04:02 +0200

    fix delete index

--- resources/views/admin/posts/partials/deleteForm.blade.php
+++ resources/views/admin/posts/partials/deleteForm.blade.php
@@ -1,7 +1,7 @@
-<form action="{{ route('admin.posts.destroy', $post) }}" method="POST" id="delete_post">
+<form action="{{ route('admin.posts.destroy', $post) }}" method="POST" id="delete_post-{{$post->id}}">
     @csrf
     @method('DELETE')
-    <button type="submit" form="delete_post" class="btn btn-danger"
+    <button type="submit" form="delete_post-{{$post->id}}" class="btn btn-danger"
     onclick="return confirm('Vuoi eliminare il Post ?')">
         Elimina
     </button>
